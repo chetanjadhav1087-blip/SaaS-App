e82e5166556991a185767f0a2791d664ed67a7dc
chetanjadhav <chetanjadhav1087@gmail.com>
2026-06-07 23:48:11 +0530

    Add Companion Card, List

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,10 +1,14 @@
+import ComapnionForm from '@/components/ComapnionForm'
 import React from 'react'
 
 function page() {
   return (
-    <div>
-      New Companions
-    </div>
+    <main className='main-lg:w1/3 min-md:w-2/3 items-center justify-center'>
+      <article className='w-full gap-4 flex flex-col'>
+        <h1>Comapnion Builder</h1>
+        <ComapnionForm />
+      </article>
+    </main>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,50 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import Cta from '@/components/Cta'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Home Page</div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard 
+          id = '123'
+          name = 'Neura the Brein Explorer'
+          topic = 'Neural Network of the brain'
+          subject = 'Science'
+          duration = {45}
+          color = 'rgb(127, 242, 215)'
+        />
+        <CompanionCard 
+          id = '234'
+          name = 'Counsty the Number Wizard'
+          topic = 'Derivatives & Integer'
+          subject = 'Maths'
+          duration = {30}
+          color = '#ddda'
+        />
+        <CompanionCard 
+          id = '345'
+          name = 'Verba the Vocabulary builder'
+          topic = 'Language'
+          subject = 'English Literature'
+          duration = {30}
+          color = 'rgb(238, 95, 95)'
+        />
+      </section> 
+
+      <section className='home-section'>
+        <CompanionsList 
+          title = 'Recently completely Sessions'
+          companions = {recentSessions}
+          classNames = 'w-2/3 max-lg:w-full'
+        />
+      </section>
+        <Cta />
+    </main>
   )
 }
 
